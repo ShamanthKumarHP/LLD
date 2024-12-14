452608ccdaaa3a07521e585963272b300347a946
Shamanth-Kumar-H-P <shamanth.kumar.h.p@ibm.com>
2024-12-14 12:24:27 +0530

    Template Design Pattern

---
+++ TemplateDesignPattern/order_process.py
@@ -0,0 +1,55 @@
+# crux: What steps to be taken is final, but behaviour of each step will vary
+
+from abc import ABC, abstractmethod
+
+# abstract class
+class OrderProcessTemplate:
+    def process_order(self):
+        self.verifyOrder()
+        self.assignDeliveryPartner()
+        self.trackOrder()
+    
+    @abstractmethod
+    def verifyOrder(self):
+        pass
+
+    @abstractmethod
+    def assignDeliveryPartner(self):
+        pass
+
+    @abstractmethod
+    def trackOrder(self):
+        pass
+
+class LocalOrderProcess(OrderProcessTemplate):
+    def verifyOrder(self):
+        print("Processing Local order: Order verified")
+        return
+    
+    def assignDeliveryPartner(self):
+        print("Processing Local Order: Assigned DP")
+
+    def trackOrder(self):
+        print("Processing Local Order: Order Tracking enabled")
+
+class InternationalOrderProcess(OrderProcessTemplate):
+    def verifyOrder(self):
+        print("Processing International order: Order verified")
+        return
+    
+    def assignDeliveryPartner(self):
+        print("Processing International Order: Assigned DP")
+
+    def trackOrder(self):
+        print("Processing International Order: Order Tracking enabled")
+    
+
+if __name__ == "__main__":
+    my_order = LocalOrderProcess()
+    my_order.process_order()
+
+    my_order = InternationalOrderProcess()
+    my_order.process_order()
+    
+        
+    
